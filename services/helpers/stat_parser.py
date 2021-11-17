8d1592dd92724be5356c74e952737317903f1591
zeroeser <maxdem4uk@yandex.ru>
2021-11-17 18:19:26 +0300

    Парсинг таблтиц талантов и статов

--- services/helpers/stat_parser.py
+++ services/helpers/stat_parser.py
@@ -2,9 +2,10 @@ import requests
 import sys
 import json
 from bs4 import BeautifulSoup
+import re
 
 
-def parse_value(value: str):
+def parse_value(value: str) -> object:
     if '+' in value:
         return [parse_value(v) for v in value.split('+')]
 
@@ -14,6 +15,10 @@ def parse_value(value: str):
         return float(val[:-1]) / 100
 
     return int(val)
+
+
+def parse_skills(value:str):
+    return
 
 
 def main(char_name):
@@ -39,14 +44,17 @@ def main(char_name):
     span_stats = live_data.find('span', {'id': 'scroll_stat'})
     table_stats = span_stats.next_sibling.next
     header_element = table_stats.contents[0]
+    print(header_element)
     data_elements = table_stats.contents[1:]
-
     headers = [elem.text for elem in header_element.contents if elem.text != 'Ascension']
     levels = []
     for level in data_elements:
+
         stats = [level.contents[0].text]
+
         for i in range(1, len(headers)):
             elem = level.contents[i]
+
             stats.append(parse_value(elem.text))
 
         levels.append(stats)
@@ -55,6 +63,18 @@ def main(char_name):
     result['stat_progression']['levels'] = levels
 
     # --- talents --
+    span_stats = live_data.find_all('div', {'class': ['skilldmgwrapper']})
+    for content in span_stats:
+        for level in content:
+            #print([elem.text for elem in level.contents])
+            for elem in level.contents:
+                print(elem.contents[2])
+            #print([level.contents.text])
+            #print(soup.find_all(r'<td>(.+?)</td>', level.contents[1]))
+
+
+            #headers = [elem.text for elem in level.contents]
+
     pass
 
     with open(f'{char_name}.json', 'w') as file:
@@ -62,4 +82,5 @@ def main(char_name):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main('xinyan')
+    #main(sys.argv[1])
